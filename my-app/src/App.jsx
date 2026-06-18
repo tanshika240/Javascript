da3e0b97985d4c24634315c1fdc671a4ca752d72
tanshika240 <tanshika240@gmail.com>
2026-06-18 12:04:11 +0530

    fix: show navbar

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Navbar from "./components/Navbar";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
@@ -7,14 +8,17 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center gap-4">
-      <button
-        onClick={() => setCount((count) => count + 1)}
-        className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition-colors cursor-pointer"
-      >
-        count is {count}
-      </button>
-    </div>
+    <>
+      <Navbar />
+      <div className="w-full h-screen flex flex-col items-center justify-center gap-4">
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition-colors cursor-pointer"
+        >
+          count is {count}
+        </button>
+      </div>
+    </>
   );
 }
 
