b14ecd89474928bd7dff8faad8217ed7e1b98368
tanshika240 <tanshika240@gmail.com>
2026-06-18 12:27:22 +0530

    use home

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [page, setPage] = useState("home");
 
   return (
@@ -13,12 +13,7 @@ function App() {
       <Navbar page={page} setPage={setPage} />
       <div className="w-full h-screen flex flex-col items-center justify-center gap-4">
         {page === "home" && (
-          <button
-            onClick={() => setCount((count) => count + 1)}
-            className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition-colors cursor-pointer"
-          >
-            count is {count}
-          </button>
+          <Home />
         )}
         {page === "about" && (
           <div className="text-center">
