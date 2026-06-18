8d8b95c1715109b1679e93e6aef57abfa6fb67c4
tanshika240 <tanshika240@gmail.com>
2026-06-18 12:31:33 +0530

    make contanct

---
+++ my-app/src/components/Contact.jsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold">Contact Page</h1>
+      <p className="mt-2 text-gray-600">
+        This is the contact page of the application.
+      </p>
+    </div>
+  );
+}
