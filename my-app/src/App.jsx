0c9740e9e50cc434bc28913063e27182c73fa192
tanshika240 <tanshika240@gmail.com>
2026-06-18 12:32:19 +0530

    remove unused imports

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,9 +3,6 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 
 function App() {
   const [page, setPage] = useState("home");
