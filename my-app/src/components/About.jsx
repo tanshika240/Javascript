53782337d6437188cdc46f3f2d34c87b3c9a51aa
tanshika240 <tanshika240@gmail.com>
2026-06-18 12:29:24 +0530

    about

---
+++ my-app/src/components/About.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+      <div className="text-center">
+        <h1 className="text-3xl font-bold">About Page</h1>
+        <p className="mt-2 text-gray-600">
+          This is the about page of the application.
+        </p>
+      </div>
+    );
+}
